d462173002c5a211da994682a4d4d542611eb171
Krishap-s <krishap.s.sreenivasan@gmail.com>
2020-08-29 12:06:57 +0530

    Add simple-peer library
    Add simple-peer library for connection purposes
    
    [Ticket: X]

--- static/src/assets/AudioCall.js
+++ static/src/assets/AudioCall.js
@@ -1,82 +1,28 @@
+import SimplePeer from 'simple-peer/simplepeer.min';
 /**
  * Represents a single call between two peers.
  * @param {Object} local stream of user
  * @param {Object} channel Communications channel between two peers.
  */
 class AudioCall {
-  constructor(stream, fchannel) {
+  constructor(isCaller, LocalStream, fchannel) {
     const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
-    this.pc = new RTCPeerConnection(configuration);
-    this.fchannel = fchannel;
-    this.oncontrolmessage = null;
-    this.onremotetrack = null;
-    this.onremotecontrolchannel = null;
-    this.onremotemessagechannel = null;
 
-    let x;
-    for (x of stream.getTracks()) {
-      this.pc.addTrack(x);
-    }
-    this.remotechannel = this.pc.createDataChannel('controller', { negotiated: true, id: 0 });
-    this.messagechannel = this.pc.createDataChannel('message', { negotiated: true, id: 1 });
-
-    this.remotechannel.onopen = () => {
-      if (this.onremotecontrolchannel) {
-        this.onremotecontrolchannel(this.remotechannel);
-      }
-    };
-
-    this.messagechannel.onopen = () => {
-      if (this.onremotemessagechannel) {
-        this.onremotemessagechannel(this.messagechannel);
-      }
-    };
-
-    this.remotechannel.onmessage = (e) => {
-      if (this.oncontrolmessage) {
-        this.oncontolmessage(JSON.parse(e.data));
-      }
-    };
-
-    this.pc.onicecandinate = (e) => {
-      this.fchannel.send(JSON.stringify({ candinate: e.candinate }));
-    };
-
-    this.pc.ontrack = (e) => {
-      this.onremotetrack(e.track);
-    };
-
-    this.fchannel.onmessage = (e) => {
-      const signal = JSON.parse(e);
-      if (signal.sdp) {
-        if (signal.type === 'offer') {
-          this.pc.setRemoteDescription(new RTCSessionDescription(signal))
-            .then(() => {
-              this.pc.createAnswer()
-                .then((desc) => {
-                  console.log(desc);
-                  this.pc.setLocalDescription(desc);
-                  this.fchannel.send(JSON.stringify(desc.toJSON()));
-                });
-            });
-        } else if (signal.candinate) {
-          /* eslint-disable no-undef */
-          this.pc.addIceCandinate(new RTCIceCandinate(signal.candinate));
-        } else {
-          this.pc.setRemoteDescription(new RTCSessionDescription(signal));
-        }
-      }
-    };
-  }
-
-  /**
- *  Creates offer to send to peer
- */
-  call() {
-    this.pc.createOffer().then((desc) => {
-      this.pc.setLocalDescription(desc);
-      this.fchannel.send(JSON.stringify(desc));
+    this.pc = new SimplePeer({
+      initiator: isCaller,
+      config: configuration,
+      trickle: true,
+      stream: LocalStream,
     });
+
+    this.pc.on('signal', (signal) => {
+      fchannel.send(JSON.stringify(signal));
+    });
+
+    fchannel.onmessage = (signalString) => {
+      const signal = JSON.parse(signalString);
+      this.pc.signal(signal);
+    };
   }
 }
 
--- static/src/views/room.js
+++ static/src/views/room.js
@@ -12,13 +12,23 @@ import playerReducer from '../assets/reducers';
 function roomView(root, Room) {
   window.playerStore = createStore(playerReducer);
   root.innerHTML = room();
+  const remoteStream = new MediaStream();
+  const aud = document.createElement('audio');
+  aud.srcObject = remoteStream;
+  root.appendChild(aud);
+  aud.play();
   Room.ontorrentlearned = (torrent) => {
-    console.log(torrent);
     // eslint-disable-next-line no-unused-vars
     const TorrentPlayer = new Player(torrent, document.getElementById('videlem'));
   };
   Room.onremoteuserset = () => {
     window.playerStore.dispatch({ type: 'SET_REMOTE' });
+  };
+
+  Room.onremotestreamadded = (stream) => {
+    console.log(stream);
+    console.log(stream.getTracks());
+    remoteStream.addTrack(stream.getTracks()[0]);
   };
 }
 
