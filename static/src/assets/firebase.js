f69febb646ae54408e4d9d4a363c44a36c73acf3
Krishap-s <krishap.s.sreenivasan@gmail.com>
2020-09-06 10:44:06 +0530

    Remove .firebase
    Remove .firebase
    
    [Ticket: X]

--- static/src/assets/firebase.js
+++ static/src/assets/firebase.js
@@ -1,11 +1,11 @@
 // Initialize firebase object.
 const firebaseConfig = {
-  apiKey: '***REMOVED***',
-  authDomain: '***REMOVED***',
-  databaseURL: '***REMOVED***',
-  projectId: '***REMOVED***',
-  appId: '***REMOVED***',
-  measurementId: '***REMOVED***',
+  apiKey: 'AIzaSyCSZOOi931rc-iEPGU-c_tqV5AVubJygIY',
+  authDomain: 'stream-with-friends.firebaseapp.com',
+  databaseURL: 'https://stream-with-friends.firebaseio.com',
+  projectId: 'stream-with-friends',
+  appId: '1:234724440621:web:21dbc3c4416bcff2c456a4',
+  measurementId: 'G-EE1BVBF4FW',
 };
 
 // eslint-disable-next-line no-undef
