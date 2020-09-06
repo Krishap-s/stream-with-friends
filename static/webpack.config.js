0672d10d3940083e536c4b08176e79f359ce79ac
Krishap-s <krishap.s.sreenivasan@gmail.com>
2020-09-06 10:54:13 +0530

    Revert "Added icons"
    This reverts commit 7f8b4d93d3b8af9cd2448772ebaab19a3d5ba353.

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
+var path = require("path");
 
 module.exports = {
-  mode: 'production',
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.(js|jsx)$/,
-        exclude: [/node_modules/, /dist/],
-        loader: 'eslint-loader',
-        options: {
-          fix: true,
-          formatter: 'stylish',
-        },
-      },
-      {
-        test: /\.(handlebars)$/,
-        loader: 'handlebars-loader',
-      },
-    ],
-  },
-};
+	mode:'production',
+	output: {
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module:{
+		rules:[
+			{
+				enforce: 'pre',
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+				options: {
+					fix:true,
+					formatter: 'stylish'
+				}
+			},
+			{
+				test:/\.(handlebars)$/,
+				loader: 'handlebars-loader',
+			},
+		]
+	}
+}
